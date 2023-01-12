01ee628b477e66b512a6f1591040e5124514c393
Amil Silahic <jinobie02@gmail.com>
2023-01-11 20:55:21 -0700

    styling updates

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ const Navigation = ({}) => {
   return (
     <>
       <div>
-        <nav className="w-full h-32 items-center flex justify-between">
+        <nav className="w-full h-16 mb-8 bg-red-500 items-center flex  lg:w-full lg:justify-between md:justify-center sm:justify-between">
           <ul className="">
             <li>
               <div>
