be228154e7ff4c947165171f8c11dc77c5c7de2d
Amil Silahic <jinobie02@gmail.com>
2023-01-11 20:51:32 -0700

    adding logo image

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,11 +1,23 @@
+import Image from "next/image";
+
 const Navigation = ({}) => {
   return (
     <>
       <div>
-        <nav className="w-full">
-          <ul className="flex justify-between items-center">
-            <li>LOGO</li>
-            <li>Secret Button</li>
+        <nav className="w-full h-32 items-center flex justify-between">
+          <ul className="">
+            <li>
+              <div>
+                {/* <Logo /> */}
+                <Image
+                  alt="logo"
+                  src="/images/logo-no-background.png"
+                  width={120}
+                  height={80}
+                  className="rounded-lg ml-5"
+                />
+              </div>
+            </li>
           </ul>
         </nav>
       </div>
