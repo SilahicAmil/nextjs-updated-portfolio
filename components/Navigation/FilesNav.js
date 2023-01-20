c75eaea2013f6f9fe8f1350ae0e2b1ec4b57df76
Amil Silahic <jinobie02@gmail.com>
2023-01-20 09:04:24 -0700

    adding file navigation

---
+++ components/Navigation/FilesNav.js
@@ -0,0 +1,26 @@
+import { FaReact } from "react-icons/fa";
+
+const FilesNav = ({}) => {
+  return (
+    <div className="flex overflow-x-auto">
+      <div className="flex h-8 items-center p-2 gap-2 w-40">
+        <FaReact />
+        Home.jsx
+      </div>
+      <div className="flex h-8 items-center p-2 gap-2 w-40">
+        <FaReact />
+        Home.jsx
+      </div>
+      <div className="flex h-8 items-center p-2 gap-2 w-40">
+        <FaReact />
+        Home.jsx
+      </div>
+      <div className="flex h-8 items-center p-2 gap-2 w-40">
+        <FaReact />
+        Home.jsx
+      </div>
+    </div>
+  );
+};
+
+export default FilesNav;
--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import { FaChevronRight, FaReact } from "react-icons/fa";
 
+import FilesNav from "./FilesNav";
 import { useState } from "react";
 
 const Navigation = ({ children }) => {
@@ -10,40 +11,43 @@ const Navigation = ({ children }) => {
   };
 
   return (
-    <div className="border-r-2 border-t-2 border-b-2 w-48 flex flex-col gap-2">
-      <p className="p-2  text-lg tracking-widest">Explorer</p>
-      <div>
-        <label
-          htmlFor="portfolio-checkbox"
-          className="flex items-center gap-1 ml-1 cursor-pointer"
-          onClick={showFilesHandler}
-        >
-          <FaChevronRight className={`${!showFiles ? `rotate-90` : null} `} />
-          PORTFOLIO
-        </label>
+    <>
+      <div className="border-r-2 border-t-2 border-b-2 w-48 flex flex-col gap-2">
+        <p className="p-2  text-lg tracking-widest">Explorer</p>
+        <div>
+          <label
+            htmlFor="portfolio-checkbox"
+            className="flex items-center gap-1 ml-1 cursor-pointer"
+            onClick={showFilesHandler}
+          >
+            <FaChevronRight className={`${!showFiles ? `rotate-90` : null} `} />
+            PORTFOLIO
+          </label>
 
-        {!showFiles ? (
-          <div className="p-2 flex flex-col justify-center text-md ">
-            <div className="flex gap-2">
-              <FaReact />
-              <p>Home.jsx</p>
+          {!showFiles ? (
+            <div className="p-2 flex flex-col justify-center text-md ">
+              <div className="flex gap-2">
+                <FaReact />
+                <p>Home.jsx</p>
+              </div>
+              <div className="flex gap-2">
+                <FaReact />
+                <p>Projects.js</p>
+              </div>
+              <div className="flex gap-2">
+                <FaReact />
+                <p>About.json</p>
+              </div>
+              <div className="flex gap-2">
+                <FaReact />
+                <p>Github.md</p>
+              </div>
             </div>
-            <div className="flex gap-2">
-              <FaReact />
-              <p>Projects.js</p>
-            </div>
-            <div className="flex gap-2">
-              <FaReact />
-              <p>About.json</p>
-            </div>
-            <div className="flex gap-2">
-              <FaReact />
-              <p>Github.md</p>
-            </div>
-          </div>
-        ) : null}
+          ) : null}
+        </div>
       </div>
-    </div>
+      <FilesNav />
+    </>
   );
 };
 
