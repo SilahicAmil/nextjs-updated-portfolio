30fd00986d33a0020ad3c61dbba9397c42569098
Amil Silahic <jinobie02@gmail.com>
2023-01-20 07:53:11 -0700

    restructuring the whole project

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import Navigation from "../components/Navigation/Navigation";
-
 const HomePage = () => {
   return (
     <>
       <div>
-        <Navigation />
         <h1 className="text-3xl bg-red-500">Home Page</h1>
       </div>
     </>
