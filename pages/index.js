3fd351cac320ce2ec7c7548a007f42fb8528703c
Amil Silahic <jinobie02@gmail.com>
2023-01-23 09:24:54 -0700

    adding some head content for SEO

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
+import Head from "next/head";
+
 const HomePage = () => {
   return (
     <>
+      <Head>
+        <title>Amil Silahic - amil.dev</title>
+        <meta
+          property="og:title"
+          content="Portfolio website for Amil Silahic. Front End Developer/Engineer. Salt Lake City, Utah"
+          key="title"
+        />
+      </Head>
       <div>
         <main className="py-2">
           <h1>Hello Everyone</h1>
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,5 +1,19 @@
+import Head from "next/head";
+
 const ProjectsPage = () => {
-  return <h1 className="">Projects Page</h1>;
+  return (
+    <>
+      <Head>
+        <title>Amil Silahic - Projects</title>
+        <meta
+          property="og:title"
+          content="Portfolio website for Amil Silahic. Front End Developer/Engineer. Salt Lake City, Utah. Projects"
+          key="title"
+        />
+      </Head>
+      <h1 className="">Projects Page</h1>
+    </>
+  );
 };
 
 export default ProjectsPage;
