e4642eaab3d2e1c93b143373327f53f857775a6d
Amil Silahic <jinobie02@gmail.com>
2023-01-24 09:55:01 -0700

    responsive styling

--- components/HomePage/SplashContent.js
+++ components/HomePage/SplashContent.js
@@ -14,7 +14,7 @@ const SplashContent = ({}) => {
             <h1 className="text-6xl  mb-3">Amil Silahic</h1>
 
             <p className="text-3xl">Full Stack Web Developer</p>
-            <div className="flex gap-8 p-2">
+            <div className="flex gap-8 lg:p-8 md:p-6 sm:p-4 xsm:p-2">
               <Link href="/projects" className="bg-red-500">
                 <button className="p-1 w-24 h-12">View Work</button>
               </Link>
