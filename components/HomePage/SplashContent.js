690bd684a21cc13510d4264cc318a51b001359ae
Amil Silahic <jinobie02@gmail.com>
2023-01-24 11:52:38 -0700

    adding logo image to splash screen

--- components/HomePage/SplashContent.js
+++ components/HomePage/SplashContent.js
@@ -1,27 +1,30 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const SplashContent = ({}) => {
   return (
     <>
-      <div className="flex flex-col h-[90vh] w-full">
-        <div className="flex flex-col m-auto w-10/12 ">
-          {/* <div className="absolute text-8xl flex flex-col opacity-50 font-bold gap-12 z-1 text-slate-700 w-4/12">
-          <h1>I BUILD</h1>
-          <h1>COOL THINGS</h1>
-        </div> */}
+      <div className="flex flex-col flex-1 h-[90vh] w-full">
+        <div className="flex flex-col m-auto w-1/2">
+          <div className="w-2/12 flex ml-auto ">
+            {/* <Image
+              alt="logo"
+              src="/images/logo-black.svg"
+              width={250}
+              height={250}
+              className="rounded-lg "
+            /> */}
+          </div>
+          <h1 className="text-6xl  mb-3">Amil Silahic</h1>
 
-          <div className="">
-            <h1 className="text-6xl  mb-3">Amil Silahic</h1>
-
-            <p className="text-3xl">Full Stack Web Developer</p>
-            <div className="flex gap-8 lg:p-8 md:p-6 sm:p-4 xsm:p-2">
-              <Link href="/projects" className="bg-red-500">
-                <button className="p-1 w-24 h-12">View Work</button>
-              </Link>
-              <Link href="/contact" className="border-lime-200 border-2">
-                <button className=" p-1 w-24 h-12">Contact</button>
-              </Link>
-            </div>
+          <p className="text-3xl">Full Stack Web Developer</p>
+          <div className="flex gap-8 lg:p-8 md:p-6 sm:p-4 xsm:p-2">
+            <Link href="/projects" className="bg-red-500">
+              <button className="p-1 w-24 h-12">View Work</button>
+            </Link>
+            <Link href="/contact" className="border-lime-200 border-2">
+              <button className=" p-1 w-24 h-12">Contact</button>
+            </Link>
           </div>
         </div>
       </div>
