0f9a71299326f6d7a72f020cd8b7083498d13bbb
Amil Silahic <jinobie02@gmail.com>
2023-01-24 12:08:00 -0700

    reverting animation changes

--- components/HomePage/SplashContent.js
+++ components/HomePage/SplashContent.js
@@ -6,15 +6,6 @@ const SplashContent = ({}) => {
     <>
       <div className="flex flex-col flex-1 h-[90vh] w-full">
         <div className="flex flex-col m-auto w-1/2">
-          <div className="w-2/12 flex ml-auto ">
-            {/* <Image
-              alt="logo"
-              src="/images/logo-black.svg"
-              width={250}
-              height={250}
-              className="rounded-lg "
-            /> */}
-          </div>
           <h1 className="text-6xl  mb-3">Amil Silahic</h1>
 
           <p className="text-3xl">Full Stack Web Developer</p>
--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,10 @@ import Layout from "../components/Layout/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
