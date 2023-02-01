f897b74ab5577f9e766b4af7da0c55cf8cbe4c83
Amil Silahic <jinobie02@gmail.com>
2023-02-01 12:21:35 -0700

    adding head to contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,9 +1,18 @@
 import ContactForm from "../../components/ContactPage/ContactForm";
 import ContactSocials from "../../components/ContactPage/ContactSocials";
+import Head from "next/head";
 
 const ContactPage = ({}) => {
   return (
     <>
+      <Head>
+        <title>Amil Silahic - Contact</title>
+        <meta
+          property="og:title"
+          content="Portfolio website for Amil Silahic. Front End Developer/Engineer. Salt Lake City, Utah. Contact Me Page"
+          key="title"
+        />
+      </Head>
       <div className="w-full flex-1 justify-center font-bold xsm:inline-block sm:flex  md:flex lg:flex">
         <ContactSocials />
 
