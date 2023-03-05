e2b6920c66c50a2aed0dfc63c8a033b72cabff18
Amil Silahic <jinobie02@gmail.com>
2023-03-04 18:36:22 -0700

    feat: :fire: Adding Toasts to contact form
    Adding Toast to contact form to show error messages or success messages

--- components/ContactPage/ContactForm.js
+++ components/ContactPage/ContactForm.js
@@ -1,16 +1,31 @@
-import { useRef, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 
 const ContactForm = ({}) => {
   const formRef = useRef();
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
 
-  const [submittedForm, setSubmittedForm] = useState(false);
   // eventually use use effect to only show
   // message sent for like 5 secnds after submit
 
   const contactFormSubmitHandler = async (e) => {
     e.preventDefault();
+
+    const nameRefValue = nameRef.current.value;
+    const emailRefValue = emailRef.current.value;
+    const messageRefValue = messageRef.current.value;
+
+    if (
+      nameRefValue.trim() === "" ||
+      emailRefValue !== "@" ||
+      messageRefValue.trim() === ""
+    ) {
+      return toast.error("Invalid Input(s)");
+    }
 
     emailjs.sendForm(
       process.env.NEXT_PUBLIC_EMAIL_SERVICE_KEY,
@@ -18,41 +33,63 @@ const ContactForm = ({}) => {
       formRef.current,
       process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY
     );
-    setSubmittedForm(true);
+
     formRef.current.reset();
+
+    setTimeout(function () {
+      return toast.success("Message Sent!");
+    }, 4000);
+
+    return toast.success("Sending Message");
   };
 
   return (
-    <div className="m-8 w-1/2">
-      <h3 className="xsm:text-md sm:text-lg md:text-xl lg:text-2xl ">
-        Or contact me directly!
-      </h3>
+    <>
+      <Toaster
+        toastOptions={{
+          duration: 4000,
+        }}
+      />
+      <div className="m-8 w-1/2">
+        <h3 className="xsm:text-md sm:text-lg md:text-xl lg:text-2xl ">
+          Or contact me directly!
+        </h3>
 
-      <form onSubmit={contactFormSubmitHandler} ref={formRef}>
-        <div className="mt-4 flex flex-col gap-6">
-          <div className="w-4/12  gap-4 lg:flex">
-            <div className="gap-2 lg:flex-col">
-              <label htmlFor="Name">Name</label>
-              <input type="text" id="from_name" name="from_name" />
-            </div>
-            <div className="gap-2 lg:flex-col  ">
-              <label htmlFor="Email">Email</label>
-              <input type="text" id="Email" name="reply_to" />
+        <form onSubmit={contactFormSubmitHandler} ref={formRef}>
+          <div className="mt-4 flex flex-col gap-6">
+            <div className="w-4/12  gap-4 lg:flex">
+              <div className="gap-2 lg:flex-col">
+                <label htmlFor="Name">Name</label>
+                <input
+                  type="text"
+                  id="from_name"
+                  name="from_name"
+                  ref={nameRef}
+                />
+              </div>
+              <div className="gap-2 lg:flex-col  ">
+                <label htmlFor="Email">Email</label>
+                <input type="text" id="Email" name="reply_to" ref={emailRef} />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="mt-4 w-8/12 flex-col lg:flex">
-          <label htmlFor="Message">Message</label>
-          <textarea id="Message" cols="30" rows="5" name="message" />
-        </div>
-        <div className="mt-4 flex h-8 w-24 items-center justify-center border-2 border-lime-200">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-
-      {submittedForm ? <div className="pt-8">Message Sent!</div> : undefined}
-    </div>
+          <div className="mt-4 w-8/12 flex-col lg:flex">
+            <label htmlFor="Message">Message</label>
+            <textarea
+              id="Message"
+              cols="30"
+              rows="5"
+              name="message"
+              ref={messageRef}
+            />
+          </div>
+          <div className="mt-4 flex h-8 w-24 items-center justify-center border-2 border-lime-200">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
